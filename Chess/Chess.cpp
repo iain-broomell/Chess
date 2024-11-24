dedf48437671fe3e3b21b8e755e5daf99b54451d
Iain Broomell <slaprabbit805@gmail.com>
2024-11-23 21:23:55 -0500

    added base project

---
+++ Chess/Chess.cpp
@@ -0,0 +1,13 @@
+/*
+Program: Chess.cpp
+Programmed by Iain Broomell
+A program which allows a user to play chess.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
